8042a5c09d39b172447ab8e793a7d08d9204ee9b
zohaibyounas <zohaiby737@gmail.com>
2024-12-29 01:13:33 +0500

    updated

--- src/pages/embedded-projects/[id].jsx
+++ src/pages/embedded-projects/[id].jsx
@@ -2,9 +2,7 @@ import Layouts from "@layouts/Layouts";
 import PageBanner from "@components/PageBanner";
 import Link from "next/link";
 import ImageView from "@components/ImageView";
-
 import { useRouter } from "next/router";
-
 import {
   getSortedProjectsData,
   getAllProjectsIds,
